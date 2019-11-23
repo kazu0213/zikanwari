bf44ac044ee2c2a666da79549ceaa1ccfb664ca8
BuildTools <unconfigured@null.spigotmc.org>
2019-11-23 12:12:37 +0900

    Improve

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // モジュールのインポート
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const time = event.timestamp();
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -27,7 +28,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text") {
-            let text = "何時限目を知りたいのかな？";
+            let text = "何時限目を知りたいのかな？"+time;
             switch (event.message.text) {
                 case 'おはよう':
                     text = "おはよう！！"
